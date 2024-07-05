a2c6c41aed61e76d91f3b945b2d14bf4c658e497
israelcruzz <israelcruz.contato@gmail.com>
2024-07-05 00:50:43 -0300

    feat: add rating comment card component

--- src/app/_components/book-card/index.tsx
+++ src/app/_components/book-card/index.tsx
@@ -16,6 +16,7 @@ import {
 } from "@/components/ui/sheet";
 import { BookOpen, BookmarkSimple } from "@phosphor-icons/react";
 import { InfoSection } from "../info-section";
+import { formatDistanceDate } from "@/_utils/format-distance-date";
 
 interface BookCardProps extends HTMLAttributes<HTMLDivElement> {
   title: string;
@@ -75,7 +76,7 @@ export const BookCard = ({
           </div>
         </main>
       </SheetTrigger>
-      <SheetContent>
+      <SheetContent className="overflow-auto flex flex-col gap-6">
         <SheetHeader className="mt-6">
           <main className="bg-[#181C2A] rounded-lg pb-4 pt-6 px-8">
             <header className="flex gap-8 border-b-[0.5px] pb-10 border-[#252D4A]">
@@ -112,14 +113,80 @@ export const BookCard = ({
               </InfoSection>
               <InfoSection sizeIcon={24} icon={BookOpen}>
                 <span className="text-sm text-gray-300">Páginas</span>
-                <h3 className="text-base text-gray-200 font-bold">
-                  {pages}
-                </h3>
+                <h3 className="text-base text-gray-200 font-bold">{pages}</h3>
               </InfoSection>
             </footer>
           </main>
         </SheetHeader>
-        <div className="grid gap-4 py-4"></div>
+        <main className="flex flex-col gap-6">
+          <div className="w-full flex justify-between">
+            <h4 className="text-sm text-gray-300">Avaliações</h4>
+            <button className="text-base font-bold text-[#8381D9]">
+              Avaliar
+            </button>
+          </div>
+
+          <div className="grid grid-cols-1 gap-6">
+            <div className="bg-[#181C2A] flex flex-col gap-5 rounded-lg p-6">
+              <header className="flex justify-between">
+                <div className="flex gap-4 items-center">
+                  <Image
+                    src="https://github.com/axelvt.png"
+                    width={40}
+                    height={40}
+                    alt=""
+                    className="rounded-full w-10 h-10"
+                  />
+
+                  <div className="flex flex-col">
+                    <h3 className="font-bold text-gray-100 text-base">Axel</h3>
+                    <span className="text-gray-300 text-sm">
+                      {formatDistanceDate(new Date())}
+                    </span>
+                  </div>
+                </div>
+
+                <RatingStars size={16} rating={4} />
+              </header>
+
+              <p className="text-sm text-gray-300">
+                Ótimo livro, comprei ele um tempo atrás e deixei parado na
+                estante, mas após um certo tempo, me deu um certo extase
+                momentaneo onde tive a vontade de ler, e com certeza foi uma
+                ótima escolha.
+              </p>
+            </div>
+            <div className="bg-[#181C2A] flex flex-col gap-5 rounded-lg p-6">
+              <header className="flex justify-between">
+                <div className="flex gap-4 items-center">
+                  <Image
+                    src="https://github.com/diego3g.png"
+                    width={40}
+                    height={40}
+                    alt=""
+                    className="rounded-full w-10 h-10"
+                  />
+
+                  <div className="flex flex-col">
+                    <h3 className="font-bold text-gray-100 text-base">Diego</h3>
+                    <span className="text-gray-300 text-sm">
+                      {formatDistanceDate(new Date())}
+                    </span>
+                  </div>
+                </div>
+
+                <RatingStars size={16} rating={4} />
+              </header>
+
+              <p className="text-sm text-gray-300">
+                Ótimo livro, comprei ele um tempo atrás e deixei parado na
+                estante, mas após um certo tempo, me deu um certo extase
+                momentaneo onde tive a vontade de ler, e com certeza foi uma
+                ótima escolha.
+              </p>
+            </div>
+          </div>
+        </main>
       </SheetContent>
     </Sheet>
   );
---
+++ src/app/_components/rating-comment--card/index.tsx
@@ -0,0 +1,46 @@
+import { formatDistanceDate } from "@/_utils/format-distance-date";
+import { RatingStars } from "../rating-stars";
+import Image from "next/image";
+
+interface RatingCommentCardProps {
+  imageUri: string;
+  name: string;
+  createdAt: Date;
+  description: string;
+  ratingCount: number;
+}
+
+export const RatingCommentCard = ({
+  imageUri,
+  name,
+  createdAt,
+  description,
+  ratingCount,
+}: RatingCommentCardProps) => {
+  return (
+    <main className="bg-[#181C2A] flex flex-col gap-5 rounded-lg p-6">
+      <header className="flex justify-between">
+        <div className="flex gap-4 items-center">
+          <Image
+            src={imageUri}
+            width={40}
+            height={40}
+            alt=""
+            className="rounded-full w-10 h-10"
+          />
+
+          <div className="flex flex-col">
+            <h3 className="font-bold text-gray-100 text-base">{name}</h3>
+            <span className="text-gray-300 text-sm">
+              {formatDistanceDate(createdAt)}
+            </span>
+          </div>
+        </div>
+
+        <RatingStars size={16} rating={ratingCount} />
+      </header>
+
+      <p className="text-sm text-gray-300">{description}</p>
+    </main>
+  );
+};
